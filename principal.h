d5d4dc9b401711d826afba67a36f10618fb4211a
Albert <hygor.marques@protonmail.com>
2025-06-11 08:35:35 -0300

    QT creation

---
+++ principal.h
@@ -0,0 +1,27 @@
+#ifndef PRINCIPAL_H
+#define PRINCIPAL_H
+
+#include <QMainWindow>
+#include <QMessageBox>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Principal;
+}
+QT_END_NAMESPACE
+
+class Principal : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Principal(QWidget *parent = nullptr);
+    ~Principal();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::Principal *ui;
+};
+#endif // PRINCIPAL_H
